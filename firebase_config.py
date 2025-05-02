78868a6c8f1b1f62da18e071ee81d04b437a91a9
Arphan Kabak Junior Bodian <127463915+daiki91@users.noreply.github.com>
2025-05-02 17:41:57 +0000

    Add files via upload

--- firebase_config.py
+++ firebase_config.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, firestore
 @st.cache_resource
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate('C:\Users\arphan\OneDrive\Bureau\etude\master\sem1\base_de_donnee\tp\projet_sql\streamlit\app-qcm-d560b-459961e4b9d5.json')
+        cred = credentials.Certificate('app-qcm-d560b-459961e4b9d5.json')
         firebase_admin.initialize_app(cred)
 
     return firestore.client()
